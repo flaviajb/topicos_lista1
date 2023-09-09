00cd3f24ea7b799172261ffcec180ed938456342
flaviajb <flaviajb2@hotmail.com>
2023-09-09 13:00:23 -0300

    Documenta funções python

--- ex1.py
+++ ex1.py
@@ -1,10 +1,49 @@
 
+""" 
+Explicação da estratégia: 
+    Realizadas validações dentro de uma lista multidimensional (listas dentro de uma lista) para validar se a linha e a coluna informada por cada jogador existe. 
+        caso a linha / coluna exista:
+            É preenchida a posição no tabuleiro, realizada a verificação de vitória ou empate. Caso hum dos dois tenha ocorrido:
+                Jogo é encerrado.
+            Caso contrário:
+                Jogador é alterado e é solicitada uma nova entrada.
+        Caso contrário:
+            entrada é inválida e é solicitada uma nova entrada válida que exista no tabuleiro
+
+Detalhamento das estruturas usadas: Para construir o tabuleiro, foram utilizadas listas dentro de uma lista principal, representando o tabuleiro
+"""
+
 def imprimir_tabuleiro(tabuleiro):
+    """Imprime o tabuleiro
+
+    Args:
+        tabuleiro (list): Lista de linhas que também são listas com os valores preenchidos nas colunas, populadas com o valor " ", "X" ou "O"
+    """
     for linha in tabuleiro:
         print(" | ".join(linha))
         print("-" * 14)
 
 def verificar_vitoria(tabuleiro, jogador):
+    """Verifica se o Jogador venceu o jogo
+
+    Para todas as condições, foi utilizada a função all() do python, para verificar se todos os elementos da iteração atendiam aos critérios validados
+    1º IF - Valida se o jogador venceu em todas as casas em uma linha (horizontal)
+    2º - Valida se o jogador venceu em todas as casas em uma coluna (vertical)
+    3º - Valida nos quadrantes diagonais se o jogador ganhou ou não
+
+    Args:
+        tabuleiro (list): Lista multidimensional representando o tabuleiro
+        jogador (str): string representando o jogador (valores possíveis: "X" ou "O")
+
+    Returns:
+        bool: Valor booleano representando vitória ou derrota
+    """
+#   [
+#       [' ', ' ', ' ', ' '],
+#       [' ', ' ', ' ', ' '],
+#       [' ', ' ', ' ', ' '],
+#       [' ', ' ', ' ', ' ']
+#   ]
     for linha in tabuleiro:
         if all(casa == jogador for casa in linha):
             return True
@@ -12,12 +51,21 @@ def verificar_vitoria(tabuleiro, jogador):
     for coluna in range(4):
         if all(tabuleiro[linha][coluna] == jogador for linha in range(4)):
             return True
+
     if all(tabuleiro[i][i] == jogador for i in range(4)) or all(tabuleiro[i][3 - i] == jogador for i in range(4)):
         return True
 
     return False
 
 def verificar_empate(tabuleiro):
+    """Verifica se o jogo empatou, ou seja, todas as casas estão preenchidas (a função é chamada após verificar que não ocorreu vitória)
+
+    Args:
+        tabuleiro (list): o tabuleiro
+
+    Returns:
+        bool: Valor booleano indicando se o jogo terminou em empate ou não ou não
+    """
     return all(casa != " " for linha in tabuleiro for casa in linha)
 
 def jogar_jogo_da_velha():
